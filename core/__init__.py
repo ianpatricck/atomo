66f888b864ee1ebbf9ee07947cd3fabb881f6b6a
ianpatricck <ianpatrick499@gmail.com>
2022-07-16 00:44:48 -0300

    import DiscordProvider

--- core/__init__.py
+++ core/__init__.py
@@ -2,7 +2,11 @@ from .database.mongo import database
 
 from .controllers.InsertQuestionController import InsertQuestionController
 from .controllers.GeneratePdfController import GeneratePdfController
+from .controllers.ListController import ListController 
+
+from .api.DiscordProvider import DiscordProvider
 
 insertQuestionController = InsertQuestionController(database)
 generatePdfController = GeneratePdfController(database)
+listController = ListController(database)
 
