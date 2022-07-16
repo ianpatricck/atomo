dce9621deec642b871ee8c45d3f885c44c97da60
ianpatricck <ianpatrick499@gmail.com>
2022-07-16 00:45:09 -0300

    try and except treatment

--- core/controllers/GeneratePdfController.py
+++ core/controllers/GeneratePdfController.py
@@ -13,46 +13,54 @@ class GeneratePdfController:
 
         data = json.loads(data)
 
-        filename = data["ramo"]
-        
-        # Extensão da classe Canvas para o arquivo
-        file = canvas.Canvas(f"build/{filename}.pdf")
+        filename = data["filename"]
 
-        discipline = self.mongoProvider["disciplines"].find_one({ "tag": data["disciplina"] })
-        branch = self.mongoProvider["branches"].find_one({ "tag": data["ramo"] })
+        try:
 
-        if ((discipline == None or discipline == len(discipline) == 0) or (branch == None or len(branch) == 0)):
+            # Extensão da classe Canvas para o arquivo
+            file = canvas.Canvas(f"build/{filename}.pdf")
+
+            discipline = self.mongoProvider["disciplines"].find_one({ "tag": data["disciplina"] })
+            branch = self.mongoProvider["branches"].find_one({ "tag": data["ramo"] })
+
+            if ((discipline == None or discipline == len(discipline) == 0) or (branch == None or len(branch) == 0)):
+                return {
+                    "status": False,
+                    "data": "Objeto não encontrato ou inexistente"
+                }
+
+            file.setTitle("Devst - " + discipline["name"])
+
+            # Cordenadas das linhas para separar o documento
+            oLine = [0, 700, 600, 700]
+
+            file.setStrokeColor("gray")
+            file.setLineWidth(.1)
+
+            file.line(oLine[0], oLine[1], oLine[2], oLine[3])
+
+            # Faz o desenho do cabeçalho do arquivo
+            self.__drawFileHeader(file, discipline, branch)
+
+            # Busca as questões por meio do dado passado e as desenha em ordem
+            self.__drawFileBody(file, data)
+
+            file.showPage()
+            file.save()
+
             return {
-                "status": False,
-                "data": "Objeto não encontrato ou inexistente"
+                "status": True,
+                "data": "Arquivo gerado com sucesso!"
             }
 
-        file.setTitle("Devst - " + discipline["name"])
-    
-        # Cordenadas das linhas para separar o documento
-        oLine = [0, 700, 600, 700]
-
-        file.setStrokeColor("gray")
-        file.setLineWidth(.1)
-
-        file.line(oLine[0], oLine[1], oLine[2], oLine[3])
-
-        # Faz o desenho do cabeçalho do arquivo
-        self.__drawFileHeader(file, discipline, branch)
-
-        # Busca as questões por meio do dado passado e as desenha em ordem
-        self.__drawFileBody(file, data)
-
-        file.showPage()
-        file.save()
-
-        return {
-            "status": True,
-            "data": "Arquivo gerado com sucesso!"
-        }
+        except:
+            return {
+                "status": False,
+                "data": "Algo deu errado, talvez ainda não existam exercícios nesse ramo"
+            }
 
     def __drawFileHeader(self, file, discipline, branch): 
-      
+
         # Ícone do arquivo
         file.drawInlineImage(f"assets/devst.png", 80, 760, 50, 50)
 
@@ -69,15 +77,15 @@ class GeneratePdfController:
 
         # Busca as questões relacionadas com a disciplína e ramo desejado e aloca em uma lista
         [questions.append(question) for question in self.mongoProvider["questions"].find({ "disciplina": data["disciplina"], "ramo": data["ramo"] })]
-    
+
         randQuestions = []
 
         # Verifica se existe questões repetidas
         count = 0
-        
+
         while (len(randQuestions) != 4):
             rand = randint(0, len(questions) - 1)
-        
+
             if questions[rand] not in randQuestions:
                 randQuestions.append(questions[rand])
 
@@ -88,7 +96,7 @@ class GeneratePdfController:
 
         # Desenha as questões no arquivo
         for index, item in enumerate(randQuestions, 1):
- 
+
             paragraphQuestion = Paragraph(str(index) + " - " + item["enunciado"] + "<br/><br/>" + "<br/>".join(item["alternativas"]))
             paragraphQuestion.wrapOn(file, 500, 500)
             paragraphQuestion.drawOn(file, rightInitialTextPosX, rightInitialTextPosY) 
@@ -100,7 +108,7 @@ class GeneratePdfController:
         # Gabarito
         for index, item in enumerate(randQuestions, 1):
             feedback.append(str(index) + " - " + item["gabarito"])
-       
+
         paragraphQuestion = Paragraph(" | ".join(feedback))
         paragraphQuestion.wrapOn(file, 150, 80)
         paragraphQuestion.drawOn(file, 450, 20)
