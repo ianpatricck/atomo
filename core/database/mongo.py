012eefaec680e05d34e6ac48b07a5a17c86f5341
ianpatricck <ianpatrick499@gmail.com>
2022-07-17 10:54:55 -0300

    os.environ on database config

--- core/database/mongo.py
+++ core/database/mongo.py
@@ -1,14 +1,16 @@
 import pymongo
 import json
+import os
 
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
 # Parametros de configuração da conexão com o MongoDB
-DB_USER = config["DB_USER"]
-DB_PASS = config["DB_PASS"]
-DB_CLUSTER = config["DB_CLUSTER"]
+
+DB_USER = config["DB_USER"] or os.environ.get("DB_USER")
+DB_PASS = config["DB_PASS"] or os.environ.get("DB_PASS")
+DB_CLUSTER = config["DB_CLUSTER"] or os.environ.get("DB_CLUSTER")
 
 # Conexão com o MongoDB
 client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASS}@{DB_CLUSTER}/?retryWrites=true&w=majority")
